74d764a1652de606fa655a52fa28224089f0cf7e
DivyaSomashekarN26 <divya.somashekar@n26.com>
2024-08-25 08:02:06 +0200

    Leet-code: Easy-Valid Parentheses

---
+++ src/main/java/org/example/ValidParentheses.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args){
+        String input = "(]";
+        System.out.println(isValid(input));
+    }
+
+    private static boolean isValid(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
